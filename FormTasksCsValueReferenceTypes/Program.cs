03324c1bb9b4a4865a2f5f19b7be2467310a862e
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2022-03-16 17:31:00 +0400

    form tasks added

--- FormTasksCsValueReferenceTypes/Program.cs
+++ FormTasksCsValueReferenceTypes/Program.cs
@@ -30,31 +30,31 @@ namespace FormTasksCsValueReferenceTypes
             #region Form-Task2
             // String massivinde verilen en uzun sozu tapan alqoritm
 
-            Console.WriteLine("Massivin olcusunu daxil edin:");
-            int size = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Massivin olcusunu daxil edin:");
+            //int size = Convert.ToInt32(Console.ReadLine());
 
-            string[] text = new string[size];
-            int maxSize = 0;
-            string max = "";
-            Console.WriteLine("Massivin elementlerini daxil edin:");
-            for (int i = 0; i < size; i++)
-            {
+            //string[] text = new string[size];
+            //int maxSize = 0;
+            //string max = "";
+            //Console.WriteLine("Massivin elementlerini daxil edin:");
+            //for (int i = 0; i < size; i++)
+            //{
 
-                text[i] = Console.ReadLine();
+            //    text[i] = Console.ReadLine();
 
-            }
+            //}
 
-            for (int i = 0; i < text.Length; i++) {
-                maxSize = text[0].Length;
-                if (text[i].Length > maxSize)
-                {
-                    
-                    maxSize = text[i].Length;
-                    max = text[i];
-            }
-                
-            }
-            Console.WriteLine("En uzun soz " + max + " sozudur.");
+            //for (int i = 0; i < text.Length; i++) {
+            //    maxSize = text[0].Length;
+            //    if (text[i].Length > maxSize)
+            //    {
+
+            //        maxSize = text[i].Length;
+            //        max = text[i];
+            //}
+
+            //}
+            //Console.WriteLine("En uzun soz " + max + " sozudur.");
             #endregion
 
             #region Form-Task3
@@ -81,6 +81,50 @@ namespace FormTasksCsValueReferenceTypes
             //    Console.WriteLine("2-nin quvveti deyil!");
             //}
             #endregion
+
+            #region Form-Task4
+            //verilmis ededler siyahisindaki cut ededlerden ibaret yeni bir array duzelden proqram
+
+            //Console.WriteLine("Ilkin massivin olcusunu daxil edin:");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //int count = 0;
+            //int[] numbers = new int[size];
+            
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.WriteLine((i+1)+"-ci elementi daxil edin:");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //int[] newNumbers = new int[count];
+            //int j = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        newNumbers[j] = numbers[i];
+            //        j++;
+            //    }
+                
+            //}
+
+            //Console.WriteLine("Yeni massivdeki elementler:");
+            //foreach(int item in newNumbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+
         }
 
     }
